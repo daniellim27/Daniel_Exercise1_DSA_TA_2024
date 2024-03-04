6689fc252301001f824be11105771981a5d61e75
daniel <144778352+daniellim27@users.noreply.github.com>
2024-03-04 18:49:21 +0700

    c

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Lion lion = new Lion("Leo", 100000, 300, "Barbary lion" );
+
+        Lion lion = new Lion("Initial Name", 0, 0, "Initial Species");
+
+
+        lion.setName("Leo");
+        lion.setAge(100000);
+        lion.setWeight(300);
+        lion.setSpecies("Barbary lion");
+
+
         System.out.println("Name: " + lion.getName());
         System.out.println("Age: " + lion.getAge());
         System.out.println("Weight: " + lion.getWeight() + " kg");
         System.out.println("Species: " + lion.getSpecies());
+
+
         lion.extinct();
     }
 }
